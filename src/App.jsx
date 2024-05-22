534f20f1e1147cc80b8efa03a8a2a255c4d22b55
Egor <egor297198@mail.ru>
2024-05-22 13:05:04 +0400

    add files

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Success from "./components/Success";
+// import  Users  from "./components/Users";
+
+function App() {
+  // Тут список пользователей: https://reqres.in/api/users
+
+  return (
+    <div className="App">
+      {/* <Users /> */}
+      <Success />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Success.jsx
@@ -0,0 +1,11 @@
+const Success = ({count}) => {
+  return (
+    <div className="success-block">
+      <img />
+      <h3></h3>
+      <p></p>
+      <button></button>
+    </div>
+  );
+};
+export default Success;
