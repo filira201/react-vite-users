93c8221e6dcf3ce6213c19316afd67a1be75a820
Egor <egor297198@mail.ru>
2024-05-22 19:05:26 +0400

    fix success svg

--- src/components/Success.jsx
+++ src/components/Success.jsx
@@ -1,7 +1,7 @@
 const Success = ({ count }) => {
   return (
     <div className="success-block">
-      <img src="/assets/success.svg" alt="Success" />
+      <img src="./assets/success.svg" alt="Success" />
       <h3>Успешно!</h3>
       <p>Всем {count} пользователям отправлено приглашение.</p>
       <button
